a31ddef036a4f20c666b306a57a792995a65136a
Debosmita Chatterjee <67039880+debosmita212@users.noreply.github.com>
2022-08-24 21:51:21 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (8.67%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,10 +1,44 @@
+//KMP->T.C=O(M+N)
 class Solution {
 public:
+    vector<int> kmpProcess(string str){
+        int n=str.length();
+        vector<int> lps(n,0);//initialized with 0
+        //start from 1
+        for(int i=1,len=0;i<n;){
+            if(str[i]==str[len]){
+                lps[i++]=++len;//as we are counting from 1 index
+            }
+            else if(len!=0){
+                len=lps[len-1];//don't increment i
+            }
+            else{
+                lps[i++]=0;
+            }
+        }
+        return lps;
+    }
     int strStr(string haystack, string needle) {
-        //check if the substring present in haystack
-        for(int i=0;i<haystack.length();i++){
-            if(haystack.substr(i,needle.length())==needle){
-                return i;
+        if(needle=="") return 0;
+        int n=haystack.length();
+        int m=needle.length();
+        
+        //Create LPS array for pattern
+        vector<int>lps=kmpProcess(needle);
+        for(int i=0,j=0;i<n;){
+            if(haystack[i]==needle[j]){
+                i++;
+                j++;
+            }
+            if(j==m)//matches return index
+                return i-j;
+            //mismatch after j matches
+            if(i<n && haystack[i]!=needle[j]){
+                //Don't match lps[0...lps[j-1]] characters,they will match anyway
+                if(j!=0)
+                    j=lps[j-1];
+                else
+                    i++; //when already j=0
             }
         }
         return -1;
