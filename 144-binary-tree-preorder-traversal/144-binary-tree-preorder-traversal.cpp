78ce662f3ba6128d858998244b338df661f94142
Debosmita Chatterjee <67039880+debosmita212@users.noreply.github.com>
2022-08-25 13:48:53 +0530

    Time: 4 ms (41.74%), Space: 8.5 MB (42.50%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
@@ -9,19 +9,25 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-//Recursive Solution-> Root-Left-Right
+//Iterative Solution-> Root-Left-Right
 class Solution {
 public:
-    void solve(vector<int> &ans,TreeNode* root){
-        //base case
-        if(root==NULL) return;
-        ans.push_back(root->val);
-        solve(ans,root->left);
-        solve(ans,root->right);
-    }
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> ans;
-        solve(ans,root);
+        stack<TreeNode*> st;
+        if(root==NULL) return ans;
+        st.push(root);
+        while(!st.empty()){
+            //pop the stack top (root)
+            TreeNode* temp=st.top();
+            ans.push_back(temp->val);
+            st.pop();
+            //LIFO so first pushing the right child then left child of the popped node
+            if(temp->right)
+                st.push(temp->right);
+            if(temp->left)
+                st.push(temp->left);
+        }
         return ans;
     }
 };
