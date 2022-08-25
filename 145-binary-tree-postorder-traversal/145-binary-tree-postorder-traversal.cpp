71c8c285e36bc9737a6d9b933e2e6259122c1dfa
Debosmita Chatterjee <67039880+debosmita212@users.noreply.github.com>
2022-08-25 14:07:21 +0530

    Time: 4 ms (42.04%), Space: 8.4 MB (63.84%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -9,19 +9,35 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-//Recursive Solution-> Left-Right-Root
+//Iterative solution-> left-right-root
 class Solution {
 public:
-    void solve(vector<int> &ans,TreeNode* root){
-        //base case
-        if(root==NULL) return;
-        solve(ans,root->left);
-        solve(ans,root->right);
-        ans.push_back(root->val);
-    }
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> ans;
-        solve(ans,root);
+        if(root==NULL) return ans;
+        stack<TreeNode*> st1,st2;
+        TreeNode* node=root;
+        //push node into stack
+        st1.push(root);
+        while(!st1.empty()){
+             //pop the top of s1 and push it to s2
+            node=st1.top();
+            st1.pop();
+            st2.push(node);
+            //if the popped node's left exists push it to s1
+            if(node->left){
+                st1.push(node->left);
+            }
+            //if the popped node's right exists push it to s1
+            if(node->right){
+                st1.push(node->right);
+            }
+        }
+        //Popping the elements and printing it LIFO
+        while(!st2.empty()){
+            ans.push_back(st2.top()->val);
+            st2.pop();
+        }
         return ans;
     }
 };
