2228627e35737146463c55a3d37146592f30959c
Debosmita Chatterjee <67039880+debosmita212@users.noreply.github.com>
2022-09-02 21:13:12 +0530

    Time: 222 ms (76.97%), Space: 104 MB (71.93%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
@@ -9,47 +9,38 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-private:
-    map<TreeNode*,int> subTreeSum;
-    int solveSum(TreeNode* root){
-        if(root==NULL) return 0;
-        else{
-            return subTreeSum[root]=root->val+solveSum(root->left)+solveSum(root->right);
-        }
+class NodeValue{
+  public:
+    int maxNode,minNode,maxSum;
+    NodeValue(int minNode,int maxNode,int maxSum){
+        this->minNode=minNode;
+        this->maxNode=maxNode;
+        this->maxSum=maxSum;
     }
-    bool isBST(TreeNode* node,int &ans){
-        if(!node) return true;
-        else if(!node->left && !node->right){
-            ans=max(ans,node->val);
-            return true;
+};
+class Solution {
+    //same as largest bst in binary postorder
+    int res=INT_MIN;
+    NodeValue solve(TreeNode* root){
+        if(root==NULL) return NodeValue(INT_MAX,INT_MIN,0);
+        
+        auto left=solve(root->left);
+        auto right=solve(root->right);
+        
+        if(left.maxNode<root->val && root->val<right.minNode){
+            //valid bst
+            res=max(res,left.maxSum+right.maxSum+root->val);
+            return NodeValue(min(root->val,left.minNode),
+                             max(root->val,right.maxNode),left.maxSum+right.maxSum
+                            +root->val);
         }
-        else{
-            bool left=isBST(node->left,ans);
-            bool right=isBST(node->right,ans);
-            if(!left || !right) return false;
-            else{
-                TreeNode* rc=node->right;
-                TreeNode* lc=node->left;
-                left=false,right=false;
-                while(lc && lc->right) lc=lc->right;
-                while(rc && rc->left) rc=rc->left;
-                if(!lc || lc->val<node->val)
-                    left=true;
-                if(!rc || rc->val>node->val)
-                    right=true;
-                if(left && right)
-                    ans=max(ans,subTreeSum[node]);
-                return left && right;
-            }
-        }
+        //Not a BST
+        return NodeValue(INT_MIN,INT_MAX,max(left.maxSum,right.maxSum));
     }
 public:
-    
     int maxSumBST(TreeNode* root) {
-        solveSum(root);
-        int ans=0;
-        isBST(root,ans);
-        return ans;
+        solve(root);
+        if(res<0) return 0;
+        return res;
     }
 };
