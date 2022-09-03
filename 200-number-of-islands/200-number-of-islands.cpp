74047367a2caa8ebd3dc289aa0aca1a07f728043
Debosmita Chatterjee <67039880+debosmita212@users.noreply.github.com>
2022-09-04 01:08:15 +0530

    Time: 52 ms (56.93%), Space: 12.3 MB (82.17%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,19 +1,24 @@
+//DFS Solution
 class Solution {
 public:
-    void dfs(vector<vector<char>> &grid,int i,int j){
-        //base case->out of bound
-        if(i<0 || j<0 || i==grid.size() || j==grid[0].size() || grid[i][j]=='0') return;
-        grid[i][j]='0'; //mark visited as 0
-        dfs(grid,i+1,j); //down
-        dfs(grid,i-1,j);  //up
-        dfs(grid,i,j-1);  //left
-        dfs(grid,i,j+1);  //right
+    void dfs(vector<vector<char>>& grid,int i,int j){
+        //base case-> out of bound or water
+        if(i<0 || j<0 || i==grid.size() || j==grid[0].size() || grid[i][j]=='0')
+            return;
+        //make it water
+        grid[i][j]='0';
+        //4 directions
+        dfs(grid,i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
         return;
     }
     int numIslands(vector<vector<char>>& grid) {
         int temp=0;
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
+                //if island
                 if(grid[i][j]=='1'){
                     dfs(grid,i,j);
                     temp++;
