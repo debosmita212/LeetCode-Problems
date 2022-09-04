d72ad91785a829775fca4668321ec397da2aab91
Debosmita Chatterjee <67039880+debosmita212@users.noreply.github.com>
2022-09-04 13:14:56 +0530

    Time: 35 ms (65.98%), Space: 13.7 MB (29.49%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,21 +1,24 @@
-//DFS Solution
 class Solution {
 public:
-    bool solve(int node,vector<int> &color,vector<vector<int>> &graph){
-        //color the node
-        if(color[node]==-1) color[node]=1;
-        //check for adjacent
-        for(auto it:graph[node]){
-            //if not colored
-            if(color[it]==-1){
-                //color adjacent with opposite color
-                color[it]=1-color[node];//if 1 then 0 ,if 0 then 1
-                //if anyone returns false, then return false
-                if(solve(it,color,graph)==false)
+    bool solve(int src,vector<int> &color,vector<vector<int>> &graph){
+        queue<int> q;
+        q.push(src);
+        color[src]=1;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            //adjacents
+            for(auto it:graph[node]){
+                if(color[it]==-1){
+                    //if not colored then color it opposite
+                    //push into queue
+                    color[it]=1-color[node];
+                    q.push(it);
+                }
+                //if adjcent nodes are same color return false
+                else if(color[it]==color[node])
                     return false;
             }
-            //if adjacent have the same colors then return false
-            else if(color[it]==color[node]) return false;
         }
         return true;
     }
@@ -23,9 +26,9 @@ public:
         int n=graph.size();
         //color array initialized with -1
         vector<int> color(n,-1);
-        //check for component
+        //for each component
         for(int i=0;i<n;i++){
-            //if not colored,color it
+            //if not colored
             if(color[i]==-1){
                 if(solve(i,color,graph)==false) return false;
             }
