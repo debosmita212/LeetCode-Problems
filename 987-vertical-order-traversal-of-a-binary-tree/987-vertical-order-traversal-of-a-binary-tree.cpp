456d4429f978104392f3e43b072114da7118a2e7
Debosmita Chatterjee <67039880+debosmita212@users.noreply.github.com>
2022-09-04 11:26:38 +0530

    Time: 3 ms (93.93%), Space: 13.7 MB (20.64%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -13,36 +13,34 @@ class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
         vector<vector<int>> ans;
-        //multisets for duplicates elements to store
-        map<int,map<int,multiset<int>>> mp; //level,vertical lines, nodes
-        queue<pair<TreeNode*,pair<int,int>>> qp; //Node,vertical,level
-        qp.push({root,{0,0}});
-        while(!qp.empty()){
-            //pop the front
-            auto temp=qp.front();
-            qp.pop();
-            //accessing node,vertical,level
+        
+        //multiset for storing duplicate nodes values
+        map<int,map<int,multiset<int>>> mp;//vertical,level,nodes
+        queue<pair<TreeNode*,pair<int,int>>> q;//node,level,vertical
+        q.push({root,{0,0}}); //pushing root with 0 level and 0 vertical
+        while(!q.empty()){
+            auto temp=q.front();
+            q.pop();
+            //Accessing node,level and vertical
             TreeNode* node=temp.first;
-            int y=temp.second.first;
-            int x=temp.second.second;
-            //insert into map
-            mp[y][x].insert(node->val);
-            //if left child exists push into queue vertical -1 level +1
+            int lev=temp.second.first;
+            int ver=temp.second.second;
+            mp[ver][lev].insert(node->val); //map insert node's value
+            //left child-> vertical=ver-1,level=lev+1
             if(node->left){
-                qp.push({node->left,{y-1,x+1}});
+                q.push({node->left,{lev+1,ver-1}});
             }
-            //if right child exists push into queue vertical +1 level +1
-            if(node->right){
-                qp.push({node->right,{y+1,x+1}});
-            }
+            //right child-> vertical=ver+1,level=lev+1
+            if(node->right)
+                q.push({node->right,{lev+1,ver+1}});
         }
         for(auto it:mp){
-            vector<int> col;
-            //for a particular vertical pushes all elements in its multiset into ans
+            vector<int> cols;
+            //for a particular vertical push all it's multiset elements into ans
             for(auto p:it.second){
-                col.insert(col.end(),p.second.begin(),p.second.end());
+                cols.insert(cols.end(),p.second.begin(),p.second.end());
             }
-            ans.push_back(col);
+            ans.push_back(cols);
         }
         return ans;
     }
