13e2a17ad5533a2dca8732786deb24fbdcd083b0
Debosmita Chatterjee <67039880+debosmita212@users.noreply.github.com>
2022-09-07 20:49:06 +0530

    Time: 32 ms (60.28%), Space: 6.6 MB (92.15%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -3,24 +3,22 @@ public:
     //Recursive approach->T.C=Exponential
     //Memoization-> T.C=O(N*M) S.C=O(N*M) +O(M+N)//Stack space
     //Tabulation-> T.C=O(N*M) S.C=O(M*N)
+    //Space Optimization-> T.C=O(N*M) S.C=O(M+N)
     int longestCommonSubsequence(string text1, string text2) {
         int n=text1.length(),m=text2.length();
-        //Shifting the coordinates
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-        //initialization
+        vector<int> prev(m+1,0),curr(m+1,0);
         for(int j=0;j<=m;j++)
-            dp[0][j]=0;
-        for(int i=0;i<=n;i++)
-            dp[i][0]=0;
+            prev[j]=0;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 //match
                 if(text1[i-1]==text2[j-1])
-                    dp[i][j]=1+dp[i-1][j-1];
+                    curr[j]=1+prev[j-1];
                 else
-                    dp[i][j]=0+max(dp[i-1][j],dp[i][j-1]);
+                    curr[j]=0+max(prev[j],curr[j-1]);
             }
+            prev=curr;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
