81f7290bd85a8975c8f6eccf8dddd587815bbe68
Debosmita Chatterjee <67039880+debosmita212@users.noreply.github.com>
2022-09-07 22:37:37 +0530

    Time: 26 ms (42.94%), Space: 6.4 MB (94.35%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -3,24 +3,26 @@ public:
     //Recursive solution-> T.C=Exponential
     //Memoization-> T.C=O(M*N) S.C=O(M*N) +O(N)
     //Tabulation-> T.C=O(M*N) S.C=O(M*N) (1 based indexing)
+    //Space Optimization-> T.C=O(M*N) S.C=O(M) (1 based indexing)
     int minDistance(string word1, string word2) {
         int n=word1.length(), m=word2.length();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-        for(int j=0;j<=m;j++) dp[0][j]=j;
-        for(int i=0;i<=n;i++) dp[i][0]=i;
+        vector<int> prev(m+1,0), curr(m+1,0);
+        for(int j=0;j<=m;j++) prev[j]=j;
         for(int i=1;i<=n;i++){
+            curr[0]=i;
             for(int j=1;j<=m;j++){
                 //matches
         if(word1[i-1]==word2[j-1]){
-            dp[i][j]=0+dp[i-1][j-1];
+            curr[j]=0+prev[j-1];
         }
         else{
-            dp[i][j]=1+min(dp[i][j-1],//insertion
-                min(dp[i-1][j],//deletion
-                dp[i-1][j-1]));//updation
+            curr[j]=1+min(curr[j-1],//insertion
+                min(prev[j],//deletion
+                prev[j-1]));//updation
         }
             }
+            prev=curr;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
