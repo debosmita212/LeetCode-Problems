2f1c02c6630d4c34947cfd266b5a90f75812fe31
Debosmita Chatterjee <67039880+debosmita212@users.noreply.github.com>
2022-09-07 22:32:26 +0530

    Time: 27 ms (41.06%), Space: 9.1 MB (40.46%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -2,25 +2,25 @@ class Solution {
 public:
     //Recursive solution-> T.C=Exponential
     //Memoization-> T.C=O(M*N) S.C=O(M*N) +O(N)
-    int solve(int i,int j,string &word1,string &word2,vector<vector<int>> &dp){
-        //base case
-        if(i<0) return j+1; //j+1 number of insertions
-        if(j<0) return i+1; //i+1 number of deletions
-        if(dp[i][j]!=-1) return dp[i][j];
-        //matches
-        if(word1[i]==word2[j]){
-            dp[i][j]=0+solve(i-1,j-1,word1,word2,dp);
-        }
-        else{
-            dp[i][j]=1+min(solve(i,j-1,word1,word2,dp),//insertion
-                min(solve(i-1,j,word1,word2,dp)//deletion
-                ,solve(i-1,j-1,word1,word2,dp)));//updation
-        }
-        return dp[i][j];
-    }
+    //Tabulation-> T.C=O(M*N) S.C=O(M*N) (1 based indexing)
     int minDistance(string word1, string word2) {
         int n=word1.length(), m=word2.length();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return solve(n-1,m-1,word1,word2,dp);
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int j=0;j<=m;j++) dp[0][j]=j;
+        for(int i=0;i<=n;i++) dp[i][0]=i;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                //matches
+        if(word1[i-1]==word2[j-1]){
+            dp[i][j]=0+dp[i-1][j-1];
+        }
+        else{
+            dp[i][j]=1+min(dp[i][j-1],//insertion
+                min(dp[i-1][j],//deletion
+                dp[i-1][j-1]));//updation
+        }
+            }
+        }
+        return dp[n][m];
     }
 };
