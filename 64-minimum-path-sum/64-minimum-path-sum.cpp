572b7e080c1993724bcc8ded3249ba5198f79583
Debosmita Chatterjee <67039880+debosmita212@users.noreply.github.com>
2022-09-08 12:21:47 +0530

    Time: 7 ms (96.19%), Space: 10.2 MB (6.40%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -2,20 +2,30 @@ class Solution {
 public:
     //Recursive Approach-> T.C=O(2^N)
     //Memoization-> T.C=O(M*N) S.C=O(M*N)+O(N)
-    int f(int i,int j,vector<vector<int>> &grid,vector<vector<int>> &dp){
-        //base case->reached destination
-        if(i==0 && j==0) return grid[0][0];
-        if(i<0 || j<0) return 1e9; //do not consider
-        if(dp[i][j]!=-1) return dp[i][j];
-        //up
-        int up=grid[i][j]+f(i-1,j,grid,dp);
-        //left
-        int left=grid[i][j]+f(i,j-1,grid,dp);
-        return dp[i][j]=min(up,left);
-    }
+     //Tabulation-> T.C=O(M*N) S.C=O(M*N)
+    
     int minPathSum(vector<vector<int>>& grid) {
         int n=grid.size(),m=grid[0].size();
         vector<vector<int>> dp(n,vector<int>(m,-1));
-        return f(n-1,m-1,grid,dp);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 && j==0)
+                    dp[i][j]=grid[0][0]; //base case
+                else{
+                    int up=grid[i][j];
+                    int left=grid[i][j];
+                    if(i>0)
+                        up+=dp[i-1][j];
+                    else
+                        up+=1e9; //out of bound
+                    if(j>0)
+                        left+=dp[i][j-1];
+                    else
+                        left+=1e9; //out of bound
+                    dp[i][j]=min(up,left);
+                }
+            }
+        }
+        return dp[n-1][m-1];
     }
 };
