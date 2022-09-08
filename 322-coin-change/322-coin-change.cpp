4643f49d6a38d3a071bb4fb72fb19169ad13b276
Debosmita Chatterjee <67039880+debosmita212@users.noreply.github.com>
2022-09-08 13:00:06 +0530

    Time: 318 ms (8.36%), Space: 49.4 MB (14.44%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -2,27 +2,30 @@ class Solution {
 public:
     //Recursive solution-> T.C=O(2^N) (Try all possible ways)
     //Memoization-> T.C=O(N*target) S.C=O(N*target)+O(target)
-    int f(int i,int amount,vector<int> &coins,vector<vector<int>> &dp){
-        //base case
-        if(i==0){
-            if(amount%coins[i]==0) return amount/coins[i];
-            else
-                return 1e9;
-        }
-        if(dp[i][amount]!=-1) return dp[i][amount];
-        //not take
-        int notTake=0+f(i-1,amount,coins,dp);
-        //take
-        int take=1e9;
-        if(coins[i]<=amount){
-            take=1+f(i,amount-coins[i],coins,dp); //infinite number of coins supply so index remains same
-        }
-        return dp[i][amount]=min(take,notTake);
-    }
+    //Tabulation-> T.C=O(N*target) S.C=O(N*target)
+    
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        int ans= f(n-1,amount,coins,dp);
+        vector<vector<int>> dp(n+1,vector<int>(amount+1,0));
+        //base case
+        dp[0][0]=0;
+        for(int i=1;i<=amount;i++){
+            if(i%coins[0]==0) dp[0][i]=i/coins[0];
+            else dp[0][i]=1e9;
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=amount;j++){
+                //not take
+        int notTake=dp[i-1][j];
+        //take
+        int take=1e9;
+        if(coins[i]<=j){
+            take=1+dp[i][j-coins[i]]; //infinite number of coins supply so index remains same
+        }
+        dp[i][j]=min(take,notTake);
+            }
+        }
+        int ans= dp[n-1][amount];
         if(ans<1e9) return ans;
         return -1;
     }
