ab7cf3dd4a228c9b97c28812c41f2e3dbdbfa760
Debosmita Chatterjee <67039880+debosmita212@users.noreply.github.com>
2022-09-20 10:45:22 +0530

    Time: 493 ms (50.16%), Space: 11.9 MB (92.72%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    //Tabulation-> T.C=O(N*M) S.C=O(N*M)
+    //Space Optimization-> T.C=O(N*M) S.C=O(M)
     
     int findLength(vector<int>& nums1, vector<int>& nums2) {
         int n=nums1.size(),m=nums2.size();
-        vector<vector<int>> dp(n,vector<int>(m,0));
+        vector<int> dp(m,0);
         int ans=0;
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+            for(int j=m-1;j>=0;j--){
                 //character matches 
                 if(nums1[i]==nums2[j]){
-                    if(i==0||j==0) dp[i][j]=1;
-                    else dp[i][j]=1+dp[i-1][j-1];
-                    ans=max(ans,dp[i][j]);
+                    if(i==0||j==0) dp[j]=1;
+                    else dp[j]=1+dp[j-1];
+                    ans=max(ans,dp[j]);
                 }
+                else dp[j]=0;
             }
         }
         return ans;
