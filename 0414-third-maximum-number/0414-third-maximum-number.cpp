b9f3eb5a81b4deadacfb38bdb3ff88bbe4563379
Debosmita Chatterjee <67039880+debosmita212@users.noreply.github.com>
2022-10-15 13:49:21 +0530

    Time: 8 ms (80.69%), Space: 9 MB (98.19%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.cpp
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -1,13 +1,30 @@
 class Solution {
 public:
-    //T.C=O(nlogn) S.C=O(N)
+    //T.C=O(n) S.C=O(1)
+    /*
+    f=2
+    s=1
+    t=LONG_MIN
+    */
     int thirdMax(vector<int>& nums) {
-        set<int> s(nums.begin(),nums.end());
-        if(s.size()<3){
-            return *max_element(s.begin(),s.end());
+        long f=LONG_MIN,s=LONG_MIN,t=LONG_MIN;
+        for(int it:nums){
+            if(it==f || it==s || it==t) continue;
+            if(it>f){
+                t=s;      
+                s=f;
+                f=it;
+            }
+            else if(it>s){
+                t=s;
+                s=it;
+            }
+            else if(it>t){
+                t=it;
+            }
         }
-        else{
-            return *next(s.begin(),s.size()-3);
-        }
+        if(t!=LONG_MIN) return (int)t;
+        else if(f!=LONG_MIN) return (int)f; //as if 3 elements not exists return the maximum
+        else return (int)s;
     }
 };
