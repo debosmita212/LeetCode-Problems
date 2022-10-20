d417918ceb164c6bcd47081f3352952e58cdcbd1
Debosmita Chatterjee <67039880+debosmita212@users.noreply.github.com>
2022-10-20 12:48:09 +0530

    Time: 8 ms (82.01%), Space: 5.9 MB (92.11%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        string ans;
+        string sym[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        int val[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        
+        //Greedy way
+        int i=0;
+        while(num!=0){
+            while(num>=val[i]){
+                num-=val[i];
+                ans+=sym[i];
+            }
+            i++;
+        }
+        return ans;
+    }
+};
