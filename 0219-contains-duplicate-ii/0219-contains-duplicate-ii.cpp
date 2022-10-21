8063a0f0a6598e504e8ec61b81c6570c1daa6e5c
Debosmita Chatterjee <67039880+debosmita212@users.noreply.github.com>
2022-10-21 12:00:29 +0530

    Time: 352 ms (57.58%), Space: 72.3 MB (91.47%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,11 +1,24 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_map<int,int> mp;
+       /* unordered_map<int,int> mp;
         for(int i=0;i<nums.size();i++){
             //comparing with the previous index
             if(mp.find(nums[i])!=mp.end() && i-mp[nums[i]]<=k) return true;
             mp[nums[i]]=i; //mapping the latest index to element
+        }
+        return false;*/
+        
+        //Unordered_set sliding window solution contains only k elements
+        unordered_set<int> s;
+        for(int i=0;i<nums.size();i++){
+            if(i>k){
+                s.erase(nums[i-k-1]); //reduce the window size
+            }
+            //the s.insert() returns a pair, the second element in the pair is set
+            //to true if a new element was inserted or false if an equivalent element already 
+            //existed
+            if(!s.insert(nums[i]).second) return true;
         }
         return false;
     }
