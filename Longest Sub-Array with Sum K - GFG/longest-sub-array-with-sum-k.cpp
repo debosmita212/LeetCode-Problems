d961d72c3566f1876580345f031934e8dc2567b9
Debosmita Chatterjee <67039880+debosmita212@users.noreply.github.com>
2023-06-11 11:17:19 +0530

    Added solution - LeetHub

--- Longest Sub-Array with Sum K - GFG/longest-sub-array-with-sum-k.cpp
+++ Longest Sub-Array with Sum K - GFG/longest-sub-array-with-sum-k.cpp
@@ -10,15 +10,20 @@ class Solution{
     { 
         int sum=0;
         int maxi=0;
-        unordered_map<int,int> mp; //sum till ith index to ith index map
+        unordered_map<int,int> mp; //sum to first occurence
         for(int i=0;i<N;i++){
             sum+=A[i];
+            //sum itself become k so array length upto this must be the maximum
             if(sum==K){
                 maxi=max(maxi,i+1);
             }
+            //if map already contains sum-k subarray sum then in between subarray sum must 
+            //be having sum k, update the maxi
             if(mp.find(sum-K)!=mp.end()){
                 maxi=max(maxi,i-mp[sum-K]);
             }
+            //if map doesn't already contains sum then only push it because the first 
+            //occurence will always gives the maximum length
             if(mp.count(sum)==0){
                 mp[sum]=i;
             }
