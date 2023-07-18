0e6aec5bff1ecb4aa2d89ff544a2f281ec4698f8
Debosmita Chatterjee <67039880+debosmita212@users.noreply.github.com>
2023-07-18 13:16:03 +0530

    Added solution - LeetHub

--- Pattern 3 - GFG/pattern-3.cpp
+++ Pattern 3 - GFG/pattern-3.cpp
@@ -7,6 +7,7 @@ using namespace std;
 class Solution {
   public:
     void printTriangle(int n) {
+        // code here
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++)
                 cout<<j<<" ";
