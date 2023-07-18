39d1d9f4ee37c5fcbaf886fdfb52734784005425
Debosmita Chatterjee <67039880+debosmita212@users.noreply.github.com>
2023-07-18 15:14:18 +0530

    Added solution - LeetHub

--- Pattern 14 - GFG/pattern-14.cpp
+++ Pattern 14 - GFG/pattern-14.cpp
@@ -7,10 +7,11 @@ using namespace std;
 class Solution {
   public:
     void printTriangle(int n) {
+        // code here
         for(int i=1;i<=n;i++){
-            for(int j=0;j<i;j++){
-                char c='A';
-                cout<<char(c+j);
+            char ch='A';
+            for(int j=1;j<=i;j++){
+                cout<<ch++;
             }
             cout<<endl;
         }
