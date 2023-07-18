d2623724ee0d10f4ce7af86759e760d0a3894e86
Debosmita Chatterjee <67039880+debosmita212@users.noreply.github.com>
2023-07-18 13:12:36 +0530

    Added solution - LeetHub

--- Pattern 1 - GFG/pattern-1.cpp
+++ Pattern 1 - GFG/pattern-1.cpp
@@ -8,9 +8,8 @@ class Solution {
   public:
     void printSquare(int n) {
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
+            for(int j=0;j<n;j++)
                 cout<<"* ";
-            }
             cout<<endl;
         }
     }
