239fa3d189419689691646f17a03c762ba866981
Debosmita Chatterjee <67039880+debosmita212@users.noreply.github.com>
2023-07-18 13:59:47 +0530

    Added solution - LeetHub

--- Pattern 10 - GFG/pattern-10.cpp
+++ Pattern 10 - GFG/pattern-10.cpp
@@ -8,15 +8,15 @@ using namespace std;
 class Solution{
 public:
     void printTriangle(int n) {
-        //upper pattern print
+        // upper triangle
         for(int i=1;i<=n;i++){
-            for(int j=0;j<i;j++)
+            for(int j=1;j<=i;j++)
                 cout<<"* ";
             cout<<endl;
         }
-        //lower pattern print
-        for(int i=n-1;i>=1;i--){
-            for(int j=0;j<i;j++)
+        //lower triangle
+        for(int i=1;i<n;i++){
+            for(int j=i;j<n;j++)
                 cout<<"* ";
             cout<<endl;
         }
